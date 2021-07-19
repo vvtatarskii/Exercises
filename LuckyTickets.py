b31859508945545b311cb83706d5f7c68eb0f9b0
Viatcheslav Tatarskii <vvtatarskii@gmail.com>
2021-07-19 14:47:19 -0600

    Added new algorithm: Version 5, and some acceleratoins related to the i4

--- LuckyTickets.py
+++ LuckyTickets.py
@@ -17,6 +17,23 @@ end = time.time()
 
 print(num)
 print((end-start)/rpt)
+print("------------------------ version 1a ------------------------------")
+start = time.time()
+for count in range(rpt):
+    num = 0
+    for i1 in range(10):
+        for i2 in range(10):
+            for i3 in range(10):
+                s1 = i1+i2+i3
+                for i4 in range(max(0,s1-18),1+min(9,s1)): #for i4 in range(10):
+                    for i5 in range(10):
+                        for i6 in range(10):
+                            if s1 == i4+i5+i6:
+                                num += 1
+end = time.time()
+
+print(num)
+print((end-start)/rpt)
 print("------------------------ version 2 ------------------------------")
 start = time.time()
 for count in range(rpt):
@@ -74,6 +91,26 @@ end = time.time()
 
 print(num)
 print((end-start)/rpt)
+print("------------------------ version 2c ------------------------------")
+start = time.time()
+for count in range(rpt):
+    num = 0
+    for i1 in range(10):
+        for i2 in range(10):
+            s1_2 = i1 + i2
+            for i3 in range(10):
+                s1 = s1_2 + i3 # i1+i2+i3
+                for i4 in range(max(0,s1-18),1+min(9,s1)): #for i4 in range(10):
+                    for i5 in range(10):
+                        s2_2 = i4 + i5
+                        for i6 in range(10):
+                            if s1 == s2_2 + i6: #i4+i5+i6:
+                                num += 1
+                                break
+end = time.time()
+
+print(num)
+print((end-start)/rpt)
 print("----------------------- version 3 ----------------------------")
 start = time.time()
 for count in range(rpt):
@@ -101,6 +138,23 @@ for count in range(rpt):
             for i3 in range(10):
                 s1 = s1_2 + i3 # i1+i2+i3
                 for i4 in range(10):
+                    for i5 in range(10):
+                        diff= s1-(i4+i5)
+                        if (diff >=0) and (diff <10):
+                            num+=1
+end = time.time()
+print(num)
+print((end-start)/rpt)
+print("----------------------- version 3b ----------------------------")
+start = time.time()
+for count in range(rpt):
+    num = 0
+    for i1 in range(10):
+        for i2 in range(10):
+            s1_2 = i1 + i2
+            for i3 in range(10):
+                s1 = s1_2 + i3 # i1+i2+i3
+                for i4 in range(max(0,s1-18),1+min(9,s1)): #for i4 in range(10):
                     for i5 in range(10):
                         diff= s1-(i4+i5)
                         if (diff >=0) and (diff <10):
@@ -142,6 +196,41 @@ for count in range(rpt):
                             diff= s1-(i4+i5)
                             if (diff >=0) and (diff <10):
                                 num+=1
+end = time.time()
+print(num)
+print((end-start)/rpt)
+print("---------------------- version 4b ----------------------------")
+start = time.time()
+for count in range(rpt):
+    num = 0
+    for i1 in range(10):
+        for i2 in range(10):
+            s1_2 = i1 +i2
+            for i3 in range(10):
+                s1 = s1_2 + i3 # i1+i2+i3
+                for i4 in range(max(0,s1-18),1+min(9,s1)): #for i4 in range(10):
+                    diff = s1 - i4
+                    if (diff >= 0) and (diff <19):
+                        for i5 in range(10):
+                            diff= s1-(i4+i5)
+                            if (diff >=0) and (diff <10):
+                                num+=1
+end = time.time()
+print(num)
+print((end-start)/rpt)
+print("---------------------- version 5 ----------------------------")
+start = time.time()
+for count in range(rpt):
+    num = 0
+    sumA =[0]*28
+    for i1 in range(10):
+        for i2 in range(10):
+            for i3 in range(10):
+                s1 = i1 + i2 + i3
+                sumA[s1] += 1
+                
+    num = sum(i**2 for i in sumA)
+               
 end = time.time()
 print(num)
 print((end-start)/rpt)
