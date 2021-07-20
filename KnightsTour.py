d96a9ba456f6779eea29b0b1344f9943e1314a7e
Viatcheslav Tatarskii <vvtatarskii@gmail.com>
2021-07-19 18:56:10 -0600

    Stated development of the class

--- KnightsTour.py
+++ KnightsTour.py
@@ -2,27 +2,30 @@ import time
 
 class ChessBoard(object):
     def __init__(self,KMap, BMap):
-        self.knights = [x.copy() for x in QMap]
+        self.knights = [x.copy() for x in KMap]
         
     def printBoard(self,s):
         # s - message/
-        rows = ['A','B','C','D','E','F','G','H']
+        rows = ['1','2','3','4','5','6','7','8']
         print(s)
-        print('  ------------------')
+        print('    -- -- -- -- -- -- -- --')
         for x in range(field_size):
             print(rows[7-x],"|",end=" ")
             for y in range(field_size):
-                if self.queens[x][y] == occupied:
-                    print("Q", end=" ")
+                k = self.knights[7-x][y]
+                if k != 0:
+                    print("%2i" % k, end=" ")
                 else:
-                    print(".",end=" ")
+                    print("  ",end=" ")
             print('|')
-        print('  ------------------')
-        print('    1 2 3 4 5 6 7 8')
+        print('    -- -- -- -- -- -- -- --')
+        print('    A B C D E F G H')
 
 field_size = 8
-QMap = [[0]*field_size for _ in field_size]
-Bmap = [[0]*field_size for _ in field_size] 
-cb = CheckBoard(KMap,BMap)
+KMap = [[0]*field_size for _ in range(field_size)]
+KMap[0][0] = 1
+KMap[1][2] =2
+BMap = [[0]*field_size for _ in range(field_size)] 
+cb = ChessBoard(KMap,BMap)
 cb.printBoard('')
 print("Done")
